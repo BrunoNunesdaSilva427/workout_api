0dec1b9f01d9e53d845fe6e13198115b948e53ec
BrunoNunesdaSilva427 <brunonunesdasilva134@gmail.com>
2025-06-11 15:01:43 -0300

    Update main.py

--- workout_api/main.py
+++ workout_api/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from workout_api.routers import api_router
+from fastapi_pagination import add_pagination  # ⬅️ Import necessário
 
 app = FastAPI(title='WorkoutApi')
 app.include_router(api_router)
+
+add_pagination(app)  # ⬅️ Ativa a paginação global
